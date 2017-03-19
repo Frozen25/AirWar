e0f4dc54356cf9f97e80d6150b61faa81c0a4ebe
Alexis <alexis_gavriel@hotmail.com>
2017-03-18 21:52:10 -0600

    comentar moviemiento del heroe vertical

---
+++ src/uncategorized/Disparo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uncategorized;
+
+/**
+ *
+ * @author Alexis
+ */
+public class Disparo extends GameObject
+{
+    //hola
+    
+    
+    
+    
+    
+@Override
+    public void update()
+    {
+
+    }    
+}
